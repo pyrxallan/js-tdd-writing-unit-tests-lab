ad688108c9a7363d01bd523f737e2f54c8f609d5
Allan Ratemo <allanisratemo@gmail.com>
2025-10-30 17:50:58 +0300

    Initial commit

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,27 @@
-// Your tests here
+const { isPalindrome } = require('../utils')
+
+describe('isPalindrome', () => {
+	test('returns true for "racecar"', () => {
+		expect(isPalindrome('racecar')).toBe(true)
+	})
+
+	test('returns false for "car"', () => {
+		expect(isPalindrome('car')).toBe(false)
+	})
+
+	test('is case-insensitive ("RaceCar")', () => {
+		expect(isPalindrome('RaceCar')).toBe(true)
+	})
+
+	test('returns false for empty string', () => {
+		expect(isPalindrome('')).toBe(false)
+	})
+
+	test('throws for non-alphabetic input', () => {
+		expect(() => isPalindrome('hello1')).toThrow()
+	})
+
+	test('throws for non-string input', () => {
+		expect(() => isPalindrome(123)).toThrow()
+	})
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,17 @@
-// Your code here
+function isPalindrome(word) {
+	if (typeof word !== 'string') {
+		throw new Error('Input must be a string')
+	}
+
+	if (word.length === 0) return false
+
+	if (!/^[A-Za-z]+$/.test(word)) {
+		throw new Error('Input must contain only alphabetic characters')
+	}
+
+	const normalized = word.toLowerCase()
+	const reversed = normalized.split('').reverse().join('')
+	return normalized === reversed
+}
+
+module.exports = { isPalindrome }
